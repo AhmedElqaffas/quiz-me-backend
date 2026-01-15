e51bf9770210482b8efb22030c8c5f297970ca33
Ahmed El-Qaffas <ahmed.elqaffas97@gmail.com>
2026-01-15 22:35:55 +0200

    use bigint primary keys instead of uuidv7

--- src/main/java/com/quizme/entities/User.java
+++ src/main/java/com/quizme/entities/User.java
@@ -1,20 +1,15 @@
 package com.quizme.entities;
 
-import com.quizme.utils.uuid.GeneratedUuidV7;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedUuidV7
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String email;
     private String username;
     @Column(name = "created_at", nullable = false)
